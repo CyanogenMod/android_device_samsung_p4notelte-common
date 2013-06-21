45185febec0572ee36bf663bfc187457556c0bc4
sbrissen <sbrissen@hotmail.com>
2013-06-21 07:32:55 -0400

    p4notelte: initial commit

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,41 @@
+#
+# Copyright (C) 2012 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# This variable is set first, so it can be overridden
+# by BoardConfigVendor.mk
+-include device/samsung/smdk4412-common/BoardCommonConfig.mk
+-include device/samsung/smdk4412-qcom-common/BoardCommonConfig.mk
+
+LOCAL_PATH := device/samsung/p4notelte-common
+
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/bluetooth
+BOARD_BLUEDROID_VENDOR_CONF := $(LOCAL_PATH)/bluetooth/vnd_p4notelte.txt
+
+# RIL
+COMMON_GLOBAL_CFLAGS += -DPROPERTY_PERMS_APPEND='{ "ril.ks.status", AID_SYSTEM, 0 },'
+
+# Camera
+COMMON_GLOBAL_CFLAGS += -DCAMERA_WITH_CITYID_PARAM
+
+# GPS
+BOARD_GPS_SET_PRIVACY := true
+
+COMMON_GLOBAL_CFLAGS += -DEXYNOS4X12_TABLET
+
+# Recovery
+# inherit from the proprietary version
+-include vendor/samsung/p4notelte-common/BoardConfigVendor.mk
