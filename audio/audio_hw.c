17d424fe694bc32fe91df849f5bd566fe48e939b
sbrissen <sbrissen@hotmail.com>
2013-10-30 08:44:16 -0400

    p4notelte: fix camcorder audio recording
    Change-Id: I165cb90762953d450b419ee9eb3d18f1e8af8204

--- audio/audio_hw.c
+++ audio/audio_hw.c
@@ -2942,8 +2942,7 @@ static const struct {
     { AUDIO_DEVICE_OUT_ALL_SCO, "sco-out" },
     { AUDIO_DEVICE_OUT_AUX_DIGITAL, "aux-digital" },
 
-    { AUDIO_DEVICE_IN_BUILTIN_MIC, "builtin-mic" },
-    { AUDIO_DEVICE_IN_BACK_MIC, "back-mic" },
+    { AUDIO_DEVICE_IN_BUILTIN_MIC | AUDIO_DEVICE_IN_BACK_MIC, "builtin-mic" },
     { AUDIO_DEVICE_IN_WIRED_HEADSET, "headset-in" },
     { AUDIO_DEVICE_IN_ALL_SCO, "sco-in" },
 };
