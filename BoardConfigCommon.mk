2ee8b8f006612745b88b4990cf606c38ce998079
sbrissen <sbrissen@hotmail.com>
2013-11-13 15:12:08 -0500

    p4notelte: fix lpm
    Change-Id: Idd2f19adec16aba59db1631dd242690d51881e4b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -39,7 +39,6 @@ COMMON_GLOBAL_CFLAGS += -DEXYNOS4X12_TABLET
 # Charging mode
 BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charging
 BOARD_BATTERY_DEVICE_NAME := "battery"
-BOARD_CHARGER_RES := device/samsung/p4notelte-common/res/charger
 
 # Recovery
 # inherit from the proprietary version
